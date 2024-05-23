98ef394e20547a2a1cd239bcbf82d10f9a9f920b
MaccBass <oxq1234@dongguk.edu>
2024-05-23 14:07:18 +0900

    Initial commit
    Initial commit

---
+++ src/main/java/com/csc/anticriminal/AntiCriminal.java
@@ -0,0 +1,17 @@
+package com.csc.anticriminal;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AntiCriminal extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
